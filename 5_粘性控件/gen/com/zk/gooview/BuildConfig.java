30a7a24a8f220c6bcc161db07ba34d176385d6e9
Administrator <Administrator@zk>
2015-09-18 15:32:13 +0800

    Git版本1.0

---
+++ 5_粘性控件/gen/com/zk/gooview/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.zk.gooview;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ 5_粘性控件/src/com/zk/gooview/MainActivity.java
@@ -0,0 +1,19 @@
+package com.zk.gooview;
+
+import com.zk.gooview.ui.GooView;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(new GooView(this));
+	}
+
+
+}

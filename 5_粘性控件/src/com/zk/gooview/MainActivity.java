30314f8050eebdd7872f5e7b23bb32a19518e6cb
Administrator <Administrator@zk>
2015-09-23 09:44:13 +0800

    2.0

--- 5_粘性控件/src/com/zk/gooview/MainActivity.java
+++ 5_粘性控件/src/com/zk/gooview/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(new GooView(this));
+		setContentView(new GooView(this)); 
 	}
 
 
